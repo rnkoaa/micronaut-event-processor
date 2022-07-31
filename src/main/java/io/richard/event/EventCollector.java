4cb57ca41775faf0210e55720bb027a77840577b
Richard Agyei <richard.agyei@gmail.com>
2022-07-31 16:34:27 -0500

    Event Record producing and listening successfully

--- src/main/java/io/richard/event/EventCollector.java
+++ src/main/java/io/richard/event/EventCollector.java
@@ -1,10 +1,7 @@
 package io.richard.event;
 
-import io.richard.event.annotations.EventRecord;
 import jakarta.inject.Singleton;
-
 import java.util.Collection;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 @Singleton
@@ -18,10 +15,6 @@ public class EventCollector {
     public void add(Object record) {
         records.add(record);
     }
-
-//    public void remove(UUID id) {
-//        records.removeIf(rec -> rec.id().equals(id));
-//    }
 
     public Collection<Object> all() {
         return records;
